3213137af8acd21478c171a2d8f29be69dbd0e14
Karen Gutierrez <karenlisethgutierrezlopez@gmail.com>
2026-09-14 13:49:50 -0500

    Feat: add create button and text titles

--- src/VentanaPrincipal.java
+++ src/VentanaPrincipal.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class VentanaPrincipal extends JFrame {
     private Biblioteca biblioteca;
@@ -9,4 +10,38 @@ public class VentanaPrincipal extends JFrame {
     private JTextField txtGenero;
     private JTextField txtAño;
     private JTextField txtCopias;
+
+    public VentanaPrincipal() {
+        biblioteca = new Biblioteca();
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(2,3,10,10));
+
+        panel.add(new JLabel("Titulo:"));
+        panel.add(txtTitulo = new JTextField());
+        panel.add(txtTitulo);
+
+        panel.add(new JLabel("Autor:"));
+        panel.add(txtAutor = new JTextField());
+        panel.add(txtAutor);
+
+        panel.add(new JLabel("Codigo:"));
+        panel.add(txtCodigo = new JTextField());
+        panel.add(txtCodigo);
+
+        panel.add(new JLabel("Genero:"));
+        panel.add(txtGenero = new JTextField());
+        panel.add(txtGenero);
+
+        panel.add(new JLabel("Año publicación"));
+        panel.add(txtAño = new JTextField());
+        panel.add(txtAño);
+
+        panel.add(new JLabel("Copias:"));
+        panel.add(txtCopias = new JTextField());
+        panel.add(txtCopias);
+
+        JButton btnCrear = new JButton("Crear Libro");
+        panel.add(btnCrear);
+    }
 }
