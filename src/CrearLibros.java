f0c9109e2141d586b067bb3c334542ad39841cf6
Karen Gutierrez <karenlisethgutierrezlopez@gmail.com>
2026-09-13 21:20:13 -0500

    Feat: add constructor

--- src/CrearLibros.java
+++ src/CrearLibros.java
@@ -6,4 +6,13 @@ public class CrearLibros {
     private String genero;
     private int añoPublicacion;
     private int copiasDisponibles;
+
+    public CrearLibros(String titulo, String autor, String codigo, String genero, int añoPublicacion, int copiasDisponibles) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.codigo = codigo;
+        this.genero = genero;
+        this.añoPublicacion = añoPublicacion;
+        this.copiasDisponibles = copiasDisponibles;
+    }
 }
