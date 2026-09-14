a43ab51f5246ba6c15fd6f7e49c0a0b05d7a5d3a
Karen Gutierrez <karenlisethgutierrezlopez@gmail.com>
2026-09-13 21:02:25 -0500

    Feat: declare data

--- src/CrearLibros.java
+++ src/CrearLibros.java
@@ -1,2 +1,9 @@
 public class CrearLibros {
+
+    private String titulo;
+    private String autor;
+    private String codigo;
+    private String genero;
+    private int añoPublicacion;
+    private int copiasDisponibles;
 }
